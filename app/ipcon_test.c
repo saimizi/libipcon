9b94f54237880d1bebeb1eb14e7f2269822a71d5
Seimizu Joukan <saimizi@gmail.com>
2017-01-19 02:29:49 +0900

    Update driver interface and test program.

--- app/ipcon_test.c
+++ app/ipcon_test.c
@@ -1,20 +1,122 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <linux/netlink.h>
+#include <netlink/netlink.h>
+#include <netlink/socket.h>
+#include <netlink/genl/genl.h>
+#include <netlink/genl/ctrl.h>
 #include <unistd.h>
 #include <pthread.h>
 #include <errno.h>
 #include <string.h>
 
+#include "ipcon.h"
 
-#define ipcon_debug(fmt, ...)	printf("[ipcon] "fmt, ##__VA_ARGS__)
-#define ipcon_info(fmt, ...)	printf("[ipcon] "fmt, ##__VA_ARGS__)
-#define ipcon_err(fmt, ...)	printf("[ipcon] "fmt, ##__VA_ARGS__)
+#define ipcon_dbg(fmt, ...)	printf("[ipcon] Debug "fmt, ##__VA_ARGS__)
+#define ipcon_info(fmt, ...)	printf("[ipcon] Info "fmt, ##__VA_ARGS__)
+#define ipcon_err(fmt, ...)	printf("[ipcon] Error "fmt, ##__VA_ARGS__)
 
+int ipcon_family;
+
+
+static int rcv_msg(struct nl_msg *msg, void *arg)
+{
+	struct nlmsghdr *nlh = nlmsg_hdr(msg);
+
+	ipcon_info("%s - %d\n", __func__, __LINE__);
+
+	if (nlh->nlmsg_type != ipcon_family)
+		return NL_STOP;
+
+
+	return NL_OK;
+}
 
 int main(int argc, char *argv[])
 {
-	return 0;
+
+	int ret = 0;
+	struct nl_sock *sk = NULL;
+	struct nl_msg *msg = NULL;
+	void *hdr = NULL;
+
+	do {
+		sk = nl_socket_alloc();
+		if (!sk) {
+			ipcon_err("sk alloc failed.\n");
+			ret = 1;
+			break;
+		}
+		ipcon_dbg("local port %u\n", nl_socket_get_local_port(sk));
+
+		ret = genl_connect(sk);
+		if (ret < 0) {
+			ipcon_err("failed to connect (%d).\n", ret);
+			ret = 1;
+			break;
+		}
+
+		ipcon_family = genl_ctrl_resolve(sk, "ipcon");
+		if (ipcon_family < 0) {
+			ipcon_err("failed to resolve ipcon (%d).\n",
+					ipcon_family);
+			ret = 1;
+			break;
+		}
+
+
+		ipcon_info("ipcon family:%d.\n", ipcon_family);
+
+		ret = nl_socket_modify_cb(sk,
+					NL_CB_VALID,
+					NL_CB_CUSTOM,
+					rcv_msg,
+					NULL);
+		if (ret < 0) {
+			ipcon_err("failed setup cb (%d).\n", ret);
+			ret = 1;
+			break;
+		}
+
+		msg = nlmsg_alloc();
+		if (!msg) {
+			ipcon_err("msg alloc failed.\n");
+			ret = 1;
+			break;
+		}
+
+		hdr = genlmsg_put(msg, 0, 0, ipcon_family,
+				IPCON_HDR_SIZE, 0, IPCON_SRV_REG, 1);
+		if (!hdr) {
+			ipcon_err("failed to setup msg.\n");
+			ret = 1;
+			break;
+		}
+
+		nla_put_u32(msg, IPCON_ATTR_MSG_TYPE, IPCON_MSG_UNICAST);
+		nla_put_u32(msg, IPCON_ATTR_SRV_PORT,
+				nl_socket_get_local_port(sk));
+		nla_put_string(msg, IPCON_ATTR_SRV_NAME, "ipcon_test");
+		nla_put_u32(msg, IPCON_ATTR_SRV_GROUP, IPCON_AUTO_GROUP);
+
+
+		ret = nl_send_auto_complete(sk, msg);
+		if (ret < 0) {
+			ipcon_err("failed to send msg(%d).\n", ret);
+			ret = 1;
+			break;
+		}
+
+		nlmsg_free(msg);
+
+		ret = nl_recvmsgs_default(sk);
+		if (ret < 0) {
+			ipcon_err("failed to rcv msg: %s(%d).\n",
+					strerror(-ret), -ret);
+			ret = 1;
+		}
+	} while (0);
+
+	nl_close(sk);
+
+	return ret;
 }
--- driver/ipcon_genl.c
+++ driver/ipcon_genl.c
@@ -11,12 +11,16 @@
 #include "ipcon_genl.h"
 #include "ipcon_dbg.h"
 
+/* Reference
+ * - inclue/net/netlink.h
+ */
+
 static struct genl_family ipcon_fam = {
 	.id = GENL_ID_GENERATE,
 	.name = IPCON_GENL_NAME,
-	.hdrsize = 0,
+	.hdrsize = IPCON_HDR_SIZE,
 	.version = 1,
-	.maxattr = IPCON_ATTR_LAST,
+	.maxattr = IPCON_ATTR_MAX,
 };
 
 enum ipcon_multicast_groups {
@@ -29,12 +33,13 @@ static const struct genl_multicast_group ipcon_mcgroups[] = {
 	[IPCON_MCGRP1] = {.name = "ipcon_user",},
 };
 
-static const struct nla_policy ipcon_policy[IPCON_ATTR_LAST] = {
-	[IPCON_ATTR_REAL_SIZE] = {.type = NLA_U32},
-	[IPCON_ATTR_PORT_ID] = {.type = NLA_U32},
+static const struct nla_policy ipcon_policy[NUM_IPCON_ATTR] = {
+	[IPCON_ATTR_MSG_TYPE] = {.type = NLA_U32},
+	[IPCON_ATTR_SRV_PORT] = {.type = NLA_U32},
 	[IPCON_ATTR_SRV_NAME] = {.type = NLA_NUL_STRING,
 				.len = IPCON_MAX_SRV_NAME_LEN - 1 },
 	[IPCON_ATTR_SRV_GROUP] = {.type = NLA_U32},
+	[IPCON_ATTR_DATA] = {.type = NLA_BINARY, .len = IPCON_MAX_MSG_LEN},
 };
 
 /*
@@ -47,6 +52,7 @@ static int ipcon_netlink_notify(struct notifier_block *nb,
 	return 0;
 }
 
+#if 0
 static int ipcon_get_selfid(struct sk_buff *skb, struct genl_info *info)
 {
 	struct sk_buff *msg;
@@ -58,7 +64,7 @@ static int ipcon_get_selfid(struct sk_buff *skb, struct genl_info *info)
 
 	hdr = genlmsg_put(msg, info->snd_portid, info->snd_seq,
 			&ipcon_fam, 0, IPCON_GET_SELFID);
-	
+
 	if (!hdr)
 		return -ENOBUFS;
 
@@ -66,26 +72,54 @@ static int ipcon_get_selfid(struct sk_buff *skb, struct genl_info *info)
 		genlmsg_cancel(msg, hdr);
 	else
 		genlmsg_end(msg, hdr);
-	
+
 	return genlmsg_reply(msg, info);
 }
+#endif
 
 static int ipcon_srv_reg(struct sk_buff *skb, struct genl_info *info)
 {
-	return 0;
+	int ret = 0;
+	char name[IPCON_MAX_SRV_NAME_LEN];
+
+	ipcon_info("ipcon_srv_reg() enter.\n");
+
+	if (!info || !info->attrs[IPCON_ATTR_MSG_TYPE] ||
+		!info->attrs[IPCON_ATTR_SRV_PORT] ||
+		!info->attrs[IPCON_ATTR_SRV_NAME] ||
+		!info->attrs[IPCON_ATTR_SRV_GROUP])
+		return -EINVAL;
+
+	ret = nlmsg_validate(info->nlhdr,
+			IPCON_HDR_SIZE,
+			IPCON_ATTR_MAX,
+			ipcon_policy);
+	if (ret < 0) {
+		ipcon_err("%s : wrong message.\n", __func__);
+		return ret;
+	}
+
+	if (info->attrs[IPCON_ATTR_MSG_TYPE])
+		ipcon_info("Msg type: %lu\n", (unsigned long)
+			nla_get_u32(info->attrs[IPCON_ATTR_MSG_TYPE]));
+	ipcon_info("Srv port: %lu\n", (unsigned long)
+			nla_get_u32(info->attrs[IPCON_ATTR_SRV_PORT]));
+	nla_strlcpy(name, info->attrs[IPCON_ATTR_SRV_NAME],
+			IPCON_MAX_SRV_NAME_LEN);
+	ipcon_info("Srv name: %s\n", name);
+	ipcon_info("Srv group: %lu\n", (unsigned long)
+			nla_get_u32(info->attrs[IPCON_ATTR_SRV_GROUP]));
+
+
+	return ret;
 }
 
 static const struct genl_ops ipcon_ops[] = {
 	{
-		.cmd = IPCON_GET_SELFID,
-		.doit = ipcon_get_selfid,
-		.policy = ipcon_policy,
-	},
-	{
 		.cmd = IPCON_SRV_REG,
 		.doit = ipcon_srv_reg,
 		.policy = ipcon_policy,
-		.flags = GENL_ADMIN_PERM,
+		/*.flags = GENL_ADMIN_PERM,*/
 	},
 };
 
