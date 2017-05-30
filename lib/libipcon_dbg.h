9c10589a4d1b51aa34ca213eeabfae4a02492959
Seimizu Joukan <joukan.seimizu@gmail.com>
2017-05-30 22:04:08 +0900

    Update debug macros.

--- lib/libipcon_dbg.h
+++ lib/libipcon_dbg.h
@@ -3,12 +3,16 @@
 #define __LIBIPCON_DEBUG_H__
 
 #define ipcon_dbg(fmt, ...)	\
-	fprintf(stderr, "[libipcon] DEBUG: "fmt, ##__VA_ARGS__)
+	fprintf(stderr, "[libipcon] %s-%d DEBUG: "fmt,\
+			__func__, __LINE__, ##__VA_ARGS__)
 #define ipcon_info(fmt, ...) \
-	fprintf(stderr, "[libipcon] INFO: "fmt, ##__VA_ARGS__)
+	fprintf(stderr, "[libipcon] %s-%d INFO: "fmt, \
+			__func__, __LINE__, ##__VA_ARGS__)
 #define ipcon_warn(fmt, ...) \
-	fprintf(stderr, "[libipcon] WARN: "fmt, ##__VA_ARGS__)
+	fprintf(stderr, "[libipcon] %s-%d WARN: "fmt, \
+			__func__, __LINE__, ##__VA_ARGS__)
 #define ipcon_err(fmt, ...) \
-	fprintf(stderr, "[libipcon] ERROR: "fmt, ##__VA_ARGS__)
+	fprintf(stderr, "[libipcon] %s-%d ERROR: "fmt, \
+			__func__, __LINE__, ##__VA_ARGS__)
 
 #endif
