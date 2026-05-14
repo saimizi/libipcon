b483a984ef3bdba7bde3e675cfdcceac32b48139
Rune <rune@joukan.local>
2026-05-15 07:04:01 +0900

    Fix: rename ipcon_group_tests_run -> ipcon_api_tests_run

--- test/ut_main.c
+++ test/ut_main.c
@@ -17,7 +17,7 @@
 
 extern int ipcon_create_handler_run(void *);
 extern int ipcon_tests_run(void *);
-extern int ipcon_group_tests_run(void *);
+extern int ipcon_api_tests_run(void *);
 
 int main(void)
 {
@@ -25,7 +25,7 @@ int main(void)
 
 	ret = (ret == 0) ? ipcon_create_handler_run(NULL) : ret;
 	ret = (ret == 0) ? ipcon_tests_run(NULL) : ret;
-	ret = (ret == 0) ? ipcon_group_tests_run(NULL) : ret;
+	ret = (ret == 0) ? ipcon_api_tests_run(NULL) : ret;
 
 	return ret;
 }
